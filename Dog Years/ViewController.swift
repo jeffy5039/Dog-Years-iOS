02cd99a961b801e8203530529e0e13aadb79066f
Jordan Byrne <jordan5039@live.com>
2014-10-07 11:28:22 +0100

    revised calculations and comments

--- Dog Years/ViewController.swift
+++ Dog Years/ViewController.swift
@@ -9,35 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //labe and textFeld for dog age conversion
     @IBOutlet weak var dogYearLabel: UILabel!
     @IBOutlet weak var EnterDogYearTextField: UITextField!
     
-    
+    //label and textField for human age conversion
     @IBOutlet weak var enterYourAgeTextView: UITextField!
     @IBOutlet weak var yourYearLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    //dog age conversion button.
     @IBAction func ConvertYearButton() {
+        //convert string to double.
         var dogYears = (EnterDogYearTextField.text as NSString).doubleValue
-        dogYearLabel.text = "your dog is \(dogYears * 6.5) years old in human years"
+        
+        //if-else for determining the calculation to use.
+        if dogYears <= 2.0 {
+            dogYearLabel.text = "your dog is \(dogYears * 10.5) years old in human years"
+        } else {
+            var ageInHumanYears: Double = 2 * 10.5;
+            dogYears -= 2
+            //NSString to format the Double
+            var age = NSString(format: "%.1f", ageInHumanYears + dogYears * 4)
+            dogYearLabel.text = "your dog is \(age) years old in human years"
+        }
+
         dogYearLabel.hidden = false
         EnterDogYearTextField.text = ""
         
     }
-
+    
+    //human age conversion button.
     @IBAction func convertToDogYearsButton() {
         var personAge = (enterYourAgeTextView.text as NSString).doubleValue
-        yourYearLabel.text = "you are \(personAge/6.5) years old in dog years"
+        if personAge <= 21 {
+            var youngAge = NSString(format: "%.1f", personAge/10.5)
+            yourYearLabel.text = "you are \(youngAge) years old in dog years"
+        } else {
+            var ageInDogYears: Double = 2
+            personAge -= 21
+            var age = NSString(format: "%.1f", ageInDogYears + personAge/4)
+            yourYearLabel.text = "you are \(age) years old in dog years"
+        }
         yourYearLabel.hidden = false
         enterYourAgeTextView.text = ""
     }
