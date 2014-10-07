697c579ce5cb6ba120f762e999a900960f013a02
Jordan Byrne <jordan5039@live.com>
2014-10-07 10:17:55 +0100

    My first rev

--- Dog Years/ViewController.swift
+++ Dog Years/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dogYearLabel: UILabel!
+    @IBOutlet weak var EnterDogYearTextField: UITextField!
+    
+    
+    @IBOutlet weak var enterYourAgeTextView: UITextField!
+    @IBOutlet weak var yourYearLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func ConvertYearButton() {
+        var dogYears = (EnterDogYearTextField.text as NSString).doubleValue
+        dogYearLabel.text = "your dog is \(dogYears * 6.5) years old in human years"
+        dogYearLabel.hidden = false
+        EnterDogYearTextField.text = ""
+        
+    }
 
+    @IBAction func convertToDogYearsButton() {
+        var personAge = (enterYourAgeTextView.text as NSString).doubleValue
+        yourYearLabel.text = "you are \(personAge/6.5) years old in dog years"
+        yourYearLabel.hidden = false
+        enterYourAgeTextView.text = ""
+    }
 }
 
